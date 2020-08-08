277beda118aa927d54585d27f22982a6f3517b99
Thomas <kakhayavelashvili@hotmail.com>
2020-08-08 05:16:25 -0700

    Buttons
    Add Insert & Remove Buttons including functionality

--- app/src/main/java/com/example/recyclerview_example/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview_example/MainActivity.kt
@@ -2,21 +2,42 @@ package com.example.recyclerview_example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private val exampleList = generateDummyList(20)
+    private val adapter = ExampleAdapter(exampleList)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val exampleList = generateDummyList(500)
-        recycler_view.adapter = ExampleAdapter(exampleList)
+
+        recycler_view.adapter = adapter
         recycler_view.layoutManager = LinearLayoutManager(this)
         recycler_view.setHasFixedSize(true)
     }
 
-    private fun generateDummyList(size: Int): List<ExampleItem>{
+    fun insertItem(view: View){
+        val index = Random.nextInt(8)
+        val newItem = ExampleItem(
+            R.drawable.ic_baseline_fiber_pin,
+            "new item at position $index",
+            "line 2"
+        )
+        exampleList.add(index, newItem)
+        adapter.notifyItemInserted(index)
+    }
+
+    fun removeItem(view: View){
+        val index: Int = Random.nextInt(8)
+        exampleList.removeAt(index)
+        adapter.notifyItemRemoved(index)
+    }
+
+    private fun generateDummyList(size: Int): ArrayList<ExampleItem>{
         val list = ArrayList<ExampleItem>()
 
         for (i in 0 until size){
